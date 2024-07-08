d2f2086ea1891eec95a42ec5ca29d82afeecbcc5
longdii <duylonghiha@gmail.com>
2024-07-08 15:26:16 +0700

    Update TraiNghiemCapTreoNuHoang_v.php

--- App/Views/Pages/TraiNghiemCapTreoNuHoang_v.php
+++ App/Views/Pages/TraiNghiemCapTreoNuHoang_v.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost./WebKhuVuiChoi/Public/Css/TraiNghiemCapTreoNuHoang.css">
-    <link rel="stylesheet" href="http://localhost/WebKhuVuiChoi/Public/Css/fonts/themify-icons/themify-icons.css">
-    <link rel="stylesheet" href="http://localhost/WebKhuVuiChoi/Public/Css/grid.css">
+    <link rel="stylesheet" href="/WebKhuVuiChoi/Public/Css/TraiNghiemCapTreoNuHoang.css">
+    <link rel="stylesheet" href="/WebKhuVuiChoi/Public/Css/fonts/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="/WebKhuVuiChoi/Public/Css/grid.css">
     <title>GioiThieu</title>
 </head>
 
@@ -14,17 +14,17 @@
     <div class="aside">
         <h1>Cáp treo Nữ hoàng và Khu đồi Huyền bí - Sun World Ha Long</h1>
         <div class="sub-aside">
-            <a href="http://localhost/WebKhuVuiChoi/TrangChuNguoiDung">Trang Chủ</a><i class="ti-angle-right"></i>
-            <a href="http://localhost/WebKhuVuiChoi/trainghiemtong">Trải Nghiệm</a><i class="ti-angle-right"></i>
+            <a href="/WebKhuVuiChoi/TrangChuNguoiDung">Trang Chủ</a><i class="ti-angle-right"></i>
+            <a href="/WebKhuVuiChoi/trainghiemtong">Trải Nghiệm</a><i class="ti-angle-right"></i>
             <div class="thanhmenu">
                 <div style="padding: 5px;"><a href="">Cáp Treo Nữ Hoàng Và Khu Đồi Huyền Bí</a><i class="ti-angle-down"></i></div>
                 <div class="menu_Trongha">
-                    <div class="menu_chu"><a href="http://localhost/WebKhuVuiChoi/TraiNghiemCongVienNuoc">Công Viên Nước</a></div>
-                    <div class="menu_chu"><a href="http://localhost/WebKhuVuiChoi/TraiNghiemCongVienRong">Công Viên Rồng</a></div>
-                    <div class="menu_chu"><a href="http://localhost/WebKhuVuiChoi/TraiNghiemAmThucVaMuaSam">Ẩm Thực Và Mua Sắn</a></div>
+                    <div class="menu_chu"><a href="/WebKhuVuiChoi/TraiNghiemCongVienNuoc">Công Viên Nước</a></div>
+                    <div class="menu_chu"><a href="/WebKhuVuiChoi/TraiNghiemCongVienRong">Công Viên Rồng</a></div>
+                    <div class="menu_chu"><a href="/WebKhuVuiChoi/TraiNghiemAmThucVaMuaSam">Ẩm Thực Và Mua Sắn</a></div>
                     <div class="menu_chu"><a href="#">Bãi biển Sun World</a></div>
-                    <div class="menu_chu"><a href="http://localhost/WebKhuVuiChoi/TraiNghiemHaiDang">Hải Đăng Sun World</a></div>
-                    <div class="menu_chu"><a href="http://localhost/WebKhuVuiChoi/TraiNghiemPhoCo">Kiosk Phố Cổ Sun World</a></div>
+                    <div class="menu_chu"><a href="/WebKhuVuiChoi/TraiNghiemHaiDang">Hải Đăng Sun World</a></div>
+                    <div class="menu_chu"><a href="/WebKhuVuiChoi/TraiNghiemPhoCo">Kiosk Phố Cổ Sun World</a></div>
                 </div>
             </div>
         </div>
@@ -48,7 +48,7 @@
                         <div class="col l-3">
 
                             <div class="content_hop">
-                                <div class="content_img" style="background-image: url(http://localhost/WebKhuVuiChoi/Public/Images/captreo_img/<?php echo $row['AnhCapTreo'] ?>);">
+                                <div class="content_img" style="background-image: url(/WebKhuVuiChoi/Public/Images/captreo_img/<?php echo $row['AnhCapTreo'] ?>);">
                                 </div>
                                 <h2><?php echo $row['TenCapTreo'] ?></h2>
                                 <div class="content_hop_tt">
