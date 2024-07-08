2cc80fcbdb37b4491d8534be31d610971ec112db
longdii <duylonghiha@gmail.com>
2024-07-08 16:19:50 +0700

    Update connectDB.php

--- App/Core/connectDB.php
+++ App/Core/connectDB.php
@@ -3,8 +3,8 @@ class connectDB{
     public $con;
     protected $servername="localhost";
     protected $username="root";
-    protected $password="";
-    protected $dbname="WebKhuVuiChoi";
+    protected $password="admin";
+    protected $dbname="db_api_qlkvc";
     function __construct(){
         $this->con=mysqli_connect($this->servername,$this->username,$this->password,$this->dbname);
         mysqli_query($this->con,"SET NAMES 'utf8'");
