0358395835f8daf401e09f076834c368f9bfbf38
longdii <duylonghiha@gmail.com>
2024-07-08 14:39:22 +0700

    Update DangNhap_v.php

--- App/Views/Pages/DangNhap_v.php
+++ App/Views/Pages/DangNhap_v.php
@@ -5,16 +5,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/WebKhuVuiChoi/Public/Css/DangNhap.css">
-    <link rel="stylesheet" href="http://localhost/WebKhuVuiChoi/Public/Css/fonts/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="/WebKhuVuiChoi/Public/Css/DangNhap.css">
+    <link rel="stylesheet" href="/WebKhuVuiChoi/Public/Css/fonts/themify-icons/themify-icons.css">
     <title>Đăng Nhập</title>
 </head>
 <body>
-    <form method="post" action="http://localhost/WebKhuVuiChoi/dangnhap/KiemTraDangNhap" >
+    <form method="post" action="/WebKhuVuiChoi/dangnhap/KiemTraDangNhap" >
     <div class="noidung">
         <div class="bang">
             <div class="anh"></div>
-              <img src="http://localhost/WebKhuVuiChoi/Public/Images/img-17y312.png" alt="">
+              <img src="/WebKhuVuiChoi/Public/Images/img-17y312.png" alt="">
         <div class="sub-noidung">
             <p class="title"><strong>Chào mừng</strong></p>
             <p class="title1"><strong>Đăng nhập</strong></p>
@@ -27,7 +27,7 @@
                 <input type="password" class="form-control" id="pass" placeholder="Nhập mật khẩu" name="txtpass" required oninvalid="ktra(this)">
             </div>
             <div class="item">
-                <a href="http://localhost/WebKhuVuiChoi/dangki">Đăng ký</a>
+                <a href="/WebKhuVuiChoi/dangki">Đăng ký</a>
                 <a href="">Quên mật khẩu</a>
             </div>
             <input id="dangnhap" type="submit" name="btndangnhap" value="Đăng Nhập">
